b0be6ce6e096de1208b5d755eed8c75739f1e82a
ilushegg <marnopolskiyi@mail.ru>
2022-08-05 12:40:41 +0300

    Made the simplest single page application

---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,5 @@
+export function AboutPage(){
+    return (
+        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus labore voluptatum facilis, nobis sequi enim, praesentium, corrupti expedita laborum adipisci earum maiores! Iste corrupti maxime, quos corporis libero molestiae maiores.</div>
+    )
+}
